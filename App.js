299b2ddd1b1ba58ccc65a97206eeb2d1956693c9
Ataes <ataes@aggarwals.com>
2022-04-06 17:43:15 -0400

    api commit

--- App.js
+++ App.js
@@ -18,3 +18,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import HomeScreen from '/Users/ataesaggarwal/code/Crystal/screens/HomeScreen.js';
+// import AddToDatabase from '/Users/ataesaggarwal/code/Crystal/screens/AddToDatabase.js';
+
+// const navigator = createStackNavigator({
+//   Home: HomeScreen,
+//   Database: AddToDatabase,
+// }, 
+// {
+//   initialRouteName: 'Home',
+//   defaultNavigationOptions: {
+//     headerShown: false 
+//   }
+// });
+
+// export default createAppContainer(navigator);
+
+
