2079c36fbce7a86751ed6a25f16e1ab2675bd58c
Ataes <ataes@aggarwals.com>
2022-04-06 17:51:36 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from '/Users/ataesaggarwal/code/Crystal/screens/HomeScreen.js';
+import AddToDatabase from '/Users/ataesaggarwal/code/Crystal/screens/AddToDatabase.js';
+
+const navigator = createStackNavigator({
+  Home: HomeScreen,
+  Database: AddToDatabase,
+}, 
+{
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    headerShown: false 
+  }
 });
 
-// import { createAppContainer } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-// import HomeScreen from '/Users/ataesaggarwal/code/Crystal/screens/HomeScreen.js';
-// import AddToDatabase from '/Users/ataesaggarwal/code/Crystal/screens/AddToDatabase.js';
-
-// const navigator = createStackNavigator({
-//   Home: HomeScreen,
-//   Database: AddToDatabase,
-// }, 
-// {
-//   initialRouteName: 'Home',
-//   defaultNavigationOptions: {
-//     headerShown: false 
-//   }
-// });
-
-// export default createAppContainer(navigator);
+export default createAppContainer(navigator);
 
 
